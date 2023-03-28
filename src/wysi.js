bfcdde6ff6042e61b1c8a71e8530909967fec22a
Momo Bassit <momo@mbassit.com>
2023-03-28 13:04:27 +0700

    Filter content when initializing and pasting
    * Remove unsupported HTML tags
    * Remove empty and comment nodes
    * Wrap text nodes in paragraph elements

--- src/wysi.js
+++ src/wysi.js
@@ -17,36 +17,55 @@
   // Default settings
   const settings = {
     el: '[data-wysi], .wysi-field',
-    tools: ['bold', 'italic', 'underline']
+    tools: ['bold', 'italic', 'underline'],
+    customTags: []
+  };
+
+  // Default allowed tags
+  const allowedTags = {
+    br: {
+      attributes: [],
+      isEmpty: true
+    }
   };
 
   // Supported tools
   const toolset = {
     paragraph: {
+      tags: ['p'],
       label: 'Paragraph',
       action: () => execCommand('formatBlock', '<p>')
     },
     quote: {
+      tags: ['blockquote'],
       label: 'Quote',
       action: () => execCommand('formatBlock', '<blockquote>')
     },
     heading: {
+      tags: ['h1', 'h2', 'h3', 'h4'],
       label: 'Heading',
       action: (level) => execCommand('formatBlock', `<h${level}>`)
     },
     bold: {
+      tags: ['strong'],
+      alias: ['b'],
       label: 'Bold',
       action: () => execCommand('bold')
     },
     italic: {
+      tags: ['em'],
+      alias: ['i'],
       label: 'Italic',
       action: () => execCommand('italic')
     },
     underline: {
+      tags: ['u'],
       label: 'Underline',
       action: () => execCommand('underline')
     },
     strikeThrough: {
+      tags: ['s'],
+      alias: ['del', 'strike'],
       label: 'Strike-through',
       action: () => execCommand('strikeThrough')
     },
@@ -67,10 +86,14 @@
       action: () => execCommand('justifyFull')
     },
     ul: {
+      tags: ['ul'],
+      subTags: ['li'],
       label: 'Bulleted list',
       action: () => execCommand('insertUnorderedList')
     },
     ol: {
+      tags: ['ol'],
+      subTags: ['li'],
       label: 'Numbered list',
       action: () => execCommand('insertOrderedList')
     },
@@ -83,14 +106,21 @@
       action: () => execCommand('outdent')
     },
     link: {
+      tags: ['a'],
+      attributes: ['id', 'name', 'href', 'target', 'onclick'],
       label: 'Link',
       action: (url) => execCommand('createLink', url)
     },
     image: {
+      tags: ['img'],
+      attributes: ['src', 'alt', 'title'],
+      isEmpty: true,
       label: 'Image',
       action: (url, text = '') => execCommand('insertHTML', `<img src="${url}" alt="${text}">`)
     },
     hr: {
+      tags: ['hr'],
+      isEmpty: true,
       label: 'Horizontal line',
       action: () => execCommand('insertHorizontalRule')
     },
@@ -99,6 +129,7 @@
       action: () => execCommand('removeFormat')
     },
     formatting: {
+      tags: ['h1', 'h2', 'h3', 'h4', 'p'],
       label: 'Select formatting'
     }
   };
@@ -111,6 +142,18 @@
     const tools = options.tools || settings.tools;
     const selector = options.el || settings.el;
     const buttons = renderToolbar(tools, options);
+
+
+    // Add custom tags if any to the allowed tags list
+    settings.customTags.forEach(custom => {
+      const attributes = custom.attributes ? custom.attributes.slice() : [];
+
+      if (custom.tags) {
+        custom.tags.forEach(tag => {
+          allowedTags[tag] = { attributes };
+        });
+      }
+    });
 
     // Append an editable region
     querySelectorAll(selector).forEach(field => {
@@ -131,7 +174,7 @@
         toolbar.innerHTML = buttons;
 
         // Set the editable region
-        editor.innerHTML = field.value; // TODO: wrap text in paragraphs
+        editor.innerHTML = prepareContent(field.value);
         editor.contentEditable = true;
 
         // Insert the editable region in the document
@@ -188,6 +231,9 @@
             '</button>'
           );
       }
+
+      // Add the current tool's tags to the list of allowed tags
+      enableTags(toolName);
     });
 
     return buttons.join('');
@@ -226,21 +272,222 @@
   }
 
   /**
+   * Prepare raw content for editing.
+   * @param {string} content The editable region's raw content.
+   * @return {string} The filter HTML content.
+   */
+  function prepareContent(content) {
+    const container = createElement('div');
+    const fragment = buildFragment(content);
+
+    filterContent(fragment);
+    wrapTextNodes(fragment);
+    cleanContent(fragment);
+    container.appendChild(fragment);
+
+    return container.innerHTML;
+  }
+
+  /**
+   * Clean up content before pasting it in an editable region.
+   * @param {object} event The browser's paste event.
+   */
+  function cleanPastedContent(event) {
+    const { region } = findEditableRegion(event.target);
+    const clipboardData = event.clipboardData;
+
+    if (region && clipboardData.types.includes('text/html')) {
+      const pasted = clipboardData.getData('text/html');
+      const content = prepareContent(pasted);
+
+      // Manually paste the cleaned content
+      execCommand('insertHTML', content);
+
+      console.log(content);
+
+      // Prevent the default paste action
+      event.preventDefault();
+    }
+  }
+
+  /**
+   * Build an html fragment from a string.
+   * @param {string} html The HTML code.
+   * @return {object} A document fragment.
+   */
+  function buildFragment(html) {
+    const template = createElement('template');
+
+    template.innerHTML = html.trim();
+    return template.content;
+  }
+
+  /**
+   * Trim whitespace from the start and end of a text.
+   * @param {string} text The text to trim.
+   * @return {string} The trimmed text.
+   */
+  function trimText(text) {
+    return text.replace(/^\s+|\s+$/g, '').trim();
+  }
+
+  /**
+   * Enable HTML tags belonging to a tool.
+   * @param {object} tool The tool object.
+   */
+  function enableTags(toolName) {
+    const tool = toolset[toolName];
+
+    if (!tool || !tool.tags) {
+      return;
+    }
+
+    const isEmpty = !!tool.isEmpty;
+    const subTags = tool.subTags || [];
+    const aliasList = tool.alias || [];
+    const alias = aliasList.length ? tool.tags[0] : undefined;
+    const tags = [...tool.tags, ...subTags, ...aliasList];
+    const attributes = tool.attributes ? tool.attributes.slice() : [];
+
+    tags.forEach(tag => {
+      allowedTags[tag] = { attributes, alias, isEmpty };
+      
+      if (!subTags.includes(tag)) {
+        allowedTags[tag].toolName = toolName;
+      }
+    });
+  }
+
+  /**
+   * Replace a DOM element with another while preserving its content.
+   * @param {object} node The element to replace.
+   * @param {string} tag The HTML tag of the new element.
+   * @param {boolean} [copyAttributes] If true, also copy the original element's attributes.
+   */
+  function replaceNode(node, tag, copyAttributes) {
+    const newElement = createElement(tag);
+    const parentNode = node.parentNode;
+    const attributes = node.attributes;
+
+    // Copy the original element's content
+    newElement.innerHTML = node.innerHTML || node.textContent;
+
+    // Copy the original element's attributes
+    if (copyAttributes && attributes) {
+      for (let i = 0; i < attributes.length; i++) {
+        newElement.setAttribute(attributes[i].name, attributes[i].value);
+      }
+    }
+
+    // Replace the element
+    parentNode.replaceChild(newElement, node);
+  }
+  
+  /**
+   * Remove unsupported HTML tags and attributes.
+   * @param {object} node The parent element to filter recursively.
+   */
+  function filterContent(node) {
+    const children = Array.from(node.childNodes);
+
+    if (!children || !children.length) {
+      return;
+    }
+
+    children.forEach(childNode => {
+      // Element nodes
+      if (childNode.nodeType === 1) {
+        // Filter recursively (deeper nodes firest)
+        filterContent(childNode);
+
+        // Check if the current element is allowed
+        const tag = childNode.tagName.toLowerCase();
+        const allowedTag = allowedTags[tag];
+        const attributes = Array.from(childNode.attributes);
+
+        if (allowedTag) {
+          // Remove attributes that are not allowed
+          for (let i = 0; i < attributes.length; i++) {
+            if (!allowedTag.attributes.includes(attributes[i].name)) {
+              childNode.removeAttribute(attributes[i].name);
+            }
+          }
+
+          // If the tag is an alias, replace it with the standard tag
+          // e.g: <b> tags will be replaced with <strong> tags
+          if (allowedTag.alias) {
+            replaceNode(childNode, allowedTag.alias, true);
+          }
+        } else {
+          // Unwrap node
+          childNode.replaceWith(...childNode.childNodes);
+        }
+      }
+    });
+  }
+
+  /**
+   * Remove empty and comment nodes.
+   * @param {object} node The parent element to filter recursively.
+   */
+  function cleanContent(node) {
+    const children = Array.from(node.childNodes);
+
+    if (!children || !children.length) {
+      return;
+    }
+
+    children.forEach(childNode => {
+      // Remove empty element nodes
+      if (childNode.nodeType === 1) {
+        // Filter recursively (deeper nodes firest)
+        cleanContent(childNode);
+
+        // Check if the element can be empty
+        const tag = childNode.tagName.toLowerCase();
+        const allowedTag = allowedTags[tag];
+
+        if (allowedTag && !allowedTag.isEmpty && trimText(childNode.innerHTML) === '') {
+          node.removeChild(childNode);
+        }
+
+      // Remove comment nodes
+      } else if (childNode.nodeType === 8) {
+        node.removeChild(childNode);
+      }
+    });
+  }
+
+  /**
+   * Wrap the child text nodes in a paragraph (non-recursively).
+   * @param {object} node The parent element of the text nodes.
+   */
+  function wrapTextNodes(node) {
+    const children = Array.from(node.childNodes);
+
+    if (!children || !children.length) {
+      return;
+    }
+
+    children.forEach(childNode => {
+      if (childNode.nodeType === 3) {
+        // Remove empty text node
+        if (trimText(childNode.textContent) === '') {
+          node.removeChild(childNode);
+
+        // Wrap text node in a paragraph
+        } else {
+          replaceNode(childNode, 'p');
+        }
+      }
+    });
+  }
+
+  /**
    * Update toolbar buttons state.
    */
   function updateToolbarState() {
     const { region, tags } = findEditableRegion(document.getSelection().anchorNode);
-    const tagToAction = {
-      'BLOCKQUOTE': 'quote',
-      'B': 'bold',
-      'I': 'italic',
-      'U': 'underline',
-      'S': 'strikeThrough',
-      'UL': 'ul',
-      'OL': 'ol',
-      'A': 'link',
-      'IMG': 'image'
-    };
 
     // Abort if the selection is not within an editable region
     if (!region) {
@@ -255,20 +502,22 @@
 
     // Update the buttons states
     tags.forEach(tag => {
-      let listBoxItem = querySelector('[data-action="paragraph"]', toolbar);
+      let listBoxItem;
 
       switch (tag) {
-        /*case 'P':
+        case 'p':
           listBoxItem = querySelector('[data-action="paragraph"]', toolbar);
-          break;*/
-        case 'H1':
-        case 'H2':
-        case 'H3':
-        case 'H4':
-          listBoxItem = querySelector(`[data-action="heading"][data-level="${tag.replace('H', '')}"]`, toolbar);
+          break;
+        case 'h1':
+        case 'h2':
+        case 'h3':
+        case 'h4':
+          listBoxItem = querySelector(`[data-action="heading"][data-level="${tag.replace('h', '')}"]`, toolbar);
           break;
         default:
-          const action = tagToAction[tag];
+          const allowedTag = allowedTags[tag];
+          const action = allowedTag ? allowedTag.toolName : undefined;
+
           if (action) {
             querySelector(`[data-action="${action}"]`, toolbar).setAttribute('aria-pressed', 'true');
           }
@@ -298,7 +547,7 @@
           region = currentNode;
           break;
         } else {
-          tags.push(tag);
+          tags.push(tag.toLowerCase());
         }
       }
 
@@ -377,6 +626,9 @@
       textarea.value = editor.innerHTML;
       dispatchEvent(textarea, 'input');
     });
+
+    // Clean up pasted content
+    addListener(document, 'paste', cleanPastedContent);
 
     // Update the toolbar buttons state
     addListener(document, 'selectionchange', updateToolbarState);

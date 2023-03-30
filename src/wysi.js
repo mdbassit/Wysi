9564822982a8fb0f6d02822f8a9aa7ac7f0492de
Momo Bassit <momo@mbassit.com>
2023-03-30 09:43:55 +0700

    Update the default tools list

--- src/wysi.js
+++ src/wysi.js
@@ -17,7 +17,10 @@
   // Default settings
   const settings = {
     el: '[data-wysi], .wysi-field',
-    tools: ['bold', 'italic', 'underline'],
+    tools: [
+      'formatting', 'separator', 'bold', 'italic', 'separator', 'alignLeft', 'alignCenter', 'alignRight', 'separator',
+      'ul', 'ol', 'separator', 'indent', 'outdent', 'separator', 'link', 'image'
+    ],
     customTags: []
   };
 

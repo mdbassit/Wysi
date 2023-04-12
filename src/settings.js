f62b00464bb82c6dd79b1bf75f4bc88873baf8da
Momo Bassit <momo@mbassit.com>
2023-04-12 11:52:28 +0700

    Add comments to explain the default settings

--- src/settings.js
+++ src/settings.js
@@ -1,9 +1,24 @@
 // Default settings
 export default {
+
+  // Default selector
   el: '[data-wysi], .wysi-field',
+
+  // Default tools in the toolbar
   tools: [
     'format', '|', 'bold', 'italic', '|', 'alignLeft', 'alignCenter', 'alignRight', '|',
     'ul', 'ol', '|', 'indent', 'outdent', '|', 'link', 'image'
   ],
-  customTags: []
+
+  //Custom tags to allow when filtering inserted content
+  customTags: [
+    /* Example:
+
+    {
+      tags: ['table', 'thead', 'tbody', 'tr', 'td', 'th'], // Tags to allow
+      attributes: ['id', 'class'] // These attributes will be permitted for all the tags above
+    }
+
+    */
+  ]
 };
